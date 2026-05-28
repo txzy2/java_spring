a22d2f3805fe7ea617794612a7965bb3ea08deed
Kamaev Anton <alk@m2lab.ru>
2026-05-28 16:58:54 +0300

    ad

---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,14 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+}
