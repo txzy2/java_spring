99f6a5a4e68e1d015bb804cb9bc645595e5ac0b4
Kamaev Anton <alk@m2lab.ru>
2026-05-28 17:10:42 +0300

    fix

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,6 +1,6 @@
 package com.example.demo.entity;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "users")
--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -4,17 +4,11 @@ import com.example.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
     Optional<User> findByUsername(String username);
 
     Optional<User> findByEmail(String email);
-
-    List<User> findByAgeGreaterThan(Integer age);
-
-    List<User> findByIsActiveTrue();
 }
