2e59d75c1ccd7fdfa8091845044950de4707cd5e
Kamaev Anton <alk@m2lab.ru>
2026-06-16 11:22:37 +0300

    получение себя

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -6,6 +6,7 @@ import com.example.demo.response.BaseApiResponse;
 import com.example.demo.response.UserResponse;
 import com.example.demo.service.UserService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,17 +23,32 @@ public class UserController {
     }
 
     @GetMapping("/")
-    public ResponseEntity<BaseApiResponse<UserResponse>> getUser(@Validated @RequestParam(name = "ext_id") UUID extId) {
+    public ResponseEntity<BaseApiResponse<UserResponse>> getUser(
+            @Validated @RequestParam(name = "ext_id") UUID extId
+    ) {
         return ResponseEntity.ok(BaseApiResponse.ok(userService.findUserByExtIdOrThrow(extId)));
     }
 
+    @GetMapping("/me")
+    public ResponseEntity<BaseApiResponse<UserResponse>> getMe(
+            @AuthenticationPrincipal UUID extId
+    ) {
+        return ResponseEntity.ok(
+                BaseApiResponse.ok(userService.findUserByExtIdOrThrow(extId))
+        );
+    }
+
     @PostMapping("/register")
-    public ResponseEntity<BaseApiResponse<UUID>> registerUser(@Validated @RequestBody UserRegisterRequest request) {
+    public ResponseEntity<BaseApiResponse<UUID>> registerUser(
+            @Validated @RequestBody UserRegisterRequest request
+    ) {
         return ResponseEntity.ok(BaseApiResponse.ok(userService.registerUser(request)));
     }
 
     @PostMapping("/login")
-    public ResponseEntity<BaseApiResponse<String>> login(@RequestBody UserLoginRequest request) {
+    public ResponseEntity<BaseApiResponse<String>> login(
+            @RequestBody UserLoginRequest request
+    ) {
         return ResponseEntity.ok(BaseApiResponse.ok(userService.login(request)));
     }
 
